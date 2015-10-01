20d75c26d5f15b4efc40b110805d040b98de1c6b
Connor Chong <crchong1@NN123-HA-20.HS.sd203.org>
2015-10-01 14:49:52 -0500

    finished

--- AvgOfThree/ChangeGiver.java
+++ AvgOfThree/ChangeGiver.java
@@ -8,7 +8,8 @@ public class ChangeGiver
         Scanner input = new Scanner(System.in);
         System.out.println("Insert dollar amount: ");
         double number = input.nextDouble();
-        int newNumber = (int) 100*number;
+        number = number*100.0;
+        int newNumber = (int) number;
         
         int dollars = newNumber / 100;
         int remainderOfQuarters = newNumber%100;
@@ -17,12 +18,13 @@ public class ChangeGiver
         int dimes = remainderOfDimes / 10;
         int remainderOfNickels = remainderOfDimes%10;
         int nickels = remainderOfNickels / 5;
-        int remainderOfPennies = ;
+        int pennies = remainderOfNickels%5;
         
-        
-        
-        
-        
-        
+        System.out.println("The number of dollars is: " + dollars);
+        System.out.println("The number of quarters is: " + quarters);
+        System.out.println("The number of dimes is: " + dimes);
+        System.out.println("The number of nickels is: " + nickels);
+        System.out.println("The number of pennies is: " + pennies);
+
     }    
 }
